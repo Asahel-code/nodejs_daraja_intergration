1714654e79746801d78fbeb8189501e3f547f2bb
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-05-03 11:50:50 +0300

    [UPDATE]: Cert addition to requests

--- src/helper/axiosUtility.js
+++ src/helper/axiosUtility.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+const path = require("path");
+const fs = require('fs');
+const https = require('https');
 const { default: axios } = require("axios");
+
+__dirname = path.resolve('./');
+
+const certificate = fs.readFileSync(path.join(__dirname, '/cert/cacert.pem'));
+
+const agent = new https.Agent({
+  rejectUnauthorized: true,
+  cert: certificate,
+});
 
 const BASE_URL = process.env.BASE_URL;
 
 const AxiosUtility = axios.create({
+  httpsAgent: agent,
   baseURL: `${BASE_URL}`,
   headers: {
     accept: 'application/json',
